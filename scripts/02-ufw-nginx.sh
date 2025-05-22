ad38e78c4af2e161babfd775eead267586a46d15
Prayoga Teguh <teguh.mow@gmail.com>
2025-05-22 10:24:09 +0700

    Update 02-ufw-nginx.sh

--- scripts/02-ufw-nginx.sh
+++ scripts/02-ufw-nginx.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# DigitalOcean Marketplace Image Validation Tool
-# © 2021 DigitalOcean LLC.
-# This code is licensed under Apache 2.0 license (see LICENSE.md for details)
-
 ufw allow ssh
 ufw allow 'Nginx Full'
 
